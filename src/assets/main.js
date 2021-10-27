4b5d942c831ef6660f99ceee8373ac82651d8f9e
Egor Dyachkov <komrad.diachckow2014@ya.ru>
2021-10-27 22:54:53 +0400

    Create custom select

--- src/assets/main.js
+++ src/assets/main.js
@@ -111,3 +111,30 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+jQuery(($) => {
+    $('.select').on('click', '.select__head', function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().removeClass('active');
+        } else {
+            $('.select__head').removeClass('open');
+            $('.select__list').addClass('active');
+            $(this).addClass('open');
+        }
+    });
+
+    $('.select').on('click', '.select__item', function () {
+        $('.select__head').removeClass('open');
+        $(this).parent().removeClass('active');
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.select').length) {
+            $('.select__head').removeClass('open');
+            $('.select__list').removeClass('active');
+        }
+    });
+});
